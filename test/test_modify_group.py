3b5467ab05d28cd5d5be76f4580fcd721b6d32f5
Tatiana554 <yata_90@mail.ru>
2024-04-19 00:39:20 +0500

    Добавлена сортировка групп при их сравнении

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,16 +5,22 @@ from model.group import Group  # импортирвание класса из д
 
 def test_modify_group_name(app):
     old_groups = app.group.get_group_list()
+    group = Group(name="New group")
     # ↓ вспомогательный метод для модифткации группы
-    app.group.modify_first_group(Group(name="New group"))
+    app.group.modify_first_group(group)
+    # запоминание (сохранение)id т.к в измененной группе id уже есть
+    group.id = old_groups [0].id
     new_groups = app.group.get_group_list()  # новый список групп
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    # сравнение новогосписка групп со страым списком
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_group_header(app):
-    old_groups = app.group.get_group_list()
-    # ↓ вспомогательный метод для модифткации группы
-    app.group.modify_first_group(Group(header="New header"))
-    new_groups = app.group.get_group_list()  # новый список групп
-    assert len(old_groups) == len(new_groups)
+#def test_modify_group_header(app):
+ #   old_groups = app.group.get_group_list()
+  #  # ↓ вспомогательный метод для модифткации группы
+   # app.group.modify_first_group(Group(header="New header"))
+    #new_groups = app.group.get_group_list()  # новый список групп
+    #assert len(old_groups) == len(new_groups)
 
